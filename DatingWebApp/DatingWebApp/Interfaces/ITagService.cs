3be15ba83b25ecddf2a448d2f731cd999551539c
Armin <muratspahica7@gmail.com>
2025-05-28 15:11:50 +0200

    Added Tag Service

---
+++ DatingWebApp/DatingWebApp/Interfaces/ITagService.cs
@@ -0,0 +1,11 @@
+﻿using DatingWebApp.DTOs;
+
+namespace DatingWebApp.Interfaces
+{
+    public interface ITagService
+    {
+        Task<IEnumerable<TagDto>> GetAllTagsAsync();
+        Task<TagDto> AddTagAsync(TagDto tagDto);
+        Task DeleteTagAsync(int id);
+    }
+}
